d0084427e725162efa611132a33fbc47f1ca5295
Y.Ukrainets <y.ukrainets@viseven.com>
2019-08-08 14:47:20 +0300

    add read content functions

--- src/cli.js
+++ src/cli.js
@@ -1,55 +1,7 @@
 const   chalk = require('chalk'),
 		fs = require('fs'),
-		commander = require('commander')
-
-function goToSlidesFolder(){
-	try {
-		process.chdir('./app');
-		console.log(`New directory: ${process.cwd()}`);
-	} catch (err) {
-		console.error(`goToSlidesFolder: ${err}`);
-	}
-};
-
-function goToImagesFolder(id=''){
-	try {
-		let newDir='./app/media/images/'+id;
-		process.chdir(newDir);
-		console.log(chalk.green(`New directory: ${process.cwd()}`));
-	} catch (err) {
-		console.error(chalk.red(`goToImagesFolder: ${err}`));
-	}
-};
-
-function goToModelsFolder(){
-	try {
-		let newDir='./app/data/models/';
-		process.chdir(newDir);
-		console.log(chalk.green(`New directory: ${process.cwd()}`));
-	} catch (err) {
-		console.error(chalk.red(`goToModelsFolder: ${err}`));
-	}
-};
-
-function goToLocalizationFolder(lang=''){
-	try {
-		let newDir='./app/i18n/'+lang;
-		process.chdir(newDir);
-		console.log(chalk.green(`New directory: ${process.cwd()}`));
-	} catch (err) {
-		console.error(chalk.red(`goToLocalizationFolder: ${err}`));
-	}
-};
-
-function getLanguage(){
-	try {
-		process.chdir('./app/settings/');
-		var content = JSON.parse(fs.readFileSync("app.json"));
-		return content.lang
-	} catch (err) {
-		console.error(chalk.red(`getLanguage: ${err}`));
-	}
-};
+		commander = require('commander'),
+		path = require('path')
 
 function getSlidesList(){
 	try {
@@ -67,9 +19,9 @@ function getSlidesList(){
 function getSlideContent(id){
 	try {
 		if(getSlidesList().includes(id)) {
-			goToSlidesFolder();
-			let content = fs.readFileSync(`${id}.html`, 'utf8');
-			return content
+			process.chdir('./app');
+			let content = fs.createReadStream(`${id}.html`, 'utf8');
+			return content.pipe(process.stdout)
 		}else{
 			throw new Error('No such slide ID');
 		}
@@ -81,7 +33,7 @@ function getSlideContent(id){
 function getSlideModel(id){
 	try {
 		if(getSlidesList().includes(id)) {
-			goToModelsFolder();
+			process.chdir('./app/data/models');
 			let content = JSON.parse(fs.readFileSync(`${id}.json`));
 			return content
 		}else{
@@ -92,6 +44,36 @@ function getSlideModel(id){
 	}
 };
 
+function getSlideLocalization(id){
+	try {
+		let language = JSON.parse(fs.readFileSync("./app/settings/app.json")).lang;
+		let locPath =`./app/i18n/${language}`;
+		if(getSlidesList().includes(id)) {
+			process.chdir(locPath);
+			let content = JSON.parse(fs.readFileSync(`${id}.json`));
+			return content
+		}else{
+			throw new Error('No such slide ID');
+		}
+	} catch (err) {
+		console.error(chalk.red(`getSlideLocalization: ${err}`));
+	}
+};
+
+function getSlideStyles(id){
+	try {
+		if(getSlidesList().includes(id)) {
+			process.chdir('./app/styles');
+			let content = fs.createReadStream(`${id}.css`);
+			return content.pipe(process.stdout)
+		}else{
+			throw new Error('No such slide ID');
+		}
+	} catch (err) {
+		console.error(chalk.red(`getSlideStyles: ${err}`));
+	}
+};
+
 
 commander.version('1.0.0').description('Filler for cobalt presentations');
 commander
@@ -99,12 +81,9 @@ commander
 	.option('-i --images <slideIDimages>','Slide ID to fill with co-image HTML,model and styles from images folder')
 	.description('Fill models,localization,styles from html file or images folder')
 	.action(() => {
-		
-		
-		
-		let directory =getSlideModel(commander.html);
-		console.log(directory);
-		
+	
+	
+	
 	});
 
 export function cli(args) {
