062ad872f78ac2802473ecf5a5f65560df404ef7
Y.Ukrainets <yuriyukrainets@gmail.com>
2019-09-09 22:22:15 +0300

    resize metod

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 const sharp = require('sharp')
 
 sharp('rainbow.png')
-    .resize({ width: 940, height: 502 })
-    .toFile('rainbow2.png');
+    .extend({
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 1,
+        background: { r: 0, g: 0, b: 0, alpha: 0 }
+    })
+    .toFile('rainb.png')
+    .then(() => {
+        console.log(`tada`)
+    })
