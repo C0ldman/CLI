c771a4153119910030d86d607af6c8ed2a2e2846
Y.Ukrainets <yuriyukrainets@gmail.com>
2019-11-04 22:51:30 +0200

    update write function

--- src/write.js
+++ src/write.js
@@ -4,43 +4,60 @@ const fs = require('fs'),
 
 export function write(path, data) {
 	if (path.slice(-4) == 'json') {
-		let file = JSON.parse(fs.readFileSync(path));
-
-		if (Array.isArray(data)) {
-			data.forEach((element) => {
-				file[element.name] = element.content;
-			})
-		} else {
-			file[data.name] = data.content;
-		};
-
-		let jsonString = JSON.stringify(file);
-
-		fs.writeFileSync(path, jsonString, (err) => {
-			if (err) {
-				console.log(`Error writing file ${path}`, err)
-			}
-		});
-
+		writeJSON(path, data);
 		console.log(chalk.hex('#28FE14')(`Successfully wrote file ${path}`));
 	} else if (path.slice(-3) == 'css') {
-		let file = fs.readFileSync(path, 'utf8');
-		let content = css.parse(file).stylesheet.rules;
-		let incomeStyle = css.parse(data).stylesheet.rules[0].declarations;
-		let incomeId = css.parse(data).stylesheet.rules[0].selectors[0];
-		content.forEach((element) => {
-			if (element.selectors[0] == incomeId) {
-				console.log(incomeStyle);
-				console.log(incomeId);
-			}
-		});
-
+		writeCSS(path, data);
+		console.log(chalk.hex('#28FE14')(`Successfully wrote file ${path}`));
 	} else {
-		fs.appendFileSync(path, data, err => {
-			if (err) {
-				console.log(`Error writing file ${path}`, err)
-			}
-		});
+		writeToEndOfFile(path, data);
 		console.log(chalk.hex('#28FE14')(`Successfully wrote file ${path}`));
 	}
 }
+
+function writeJSON(path, data) {
+	let file = JSON.parse(fs.readFileSync(path));
+
+	if (Array.isArray(data)) {
+		data.forEach((element) => {
+			file[element.name] = element.content;
+		})
+	} else {
+		file[data.name] = data.content;
+	};
+
+	let jsonString = JSON.stringify(file);
+
+	fs.writeFileSync(path, jsonString, (err) => {
+		if (err) {
+			console.log(`Error writing file ${path}`, err)
+		}
+	});
+}
+
+function writeCSS(path, data) {
+	let file = css.parse(fs.readFileSync(path, 'utf8'));
+	let styles = file.stylesheet.rules;
+	let incomeStyle = css.parse(data).stylesheet.rules[0].declarations;
+	let incomeId = css.parse(data).stylesheet.rules[0].selectors[0];
+
+
+	styles.forEach((element) => {
+		if (element.selectors[0] == incomeId) {
+			incomeStyle.forEach((newStyle) => {
+				
+			})
+		}
+	});
+
+}
+
+function writeToEndOfFile(path, data) {
+	fs.appendFileSync(path, data, err => {
+		if (err) {
+			console.log(`Error writing file ${path}`, err)
+		}
+	})
+}
+
+function hasProperty(elementProp) { return elementProp.property == 'width' }
