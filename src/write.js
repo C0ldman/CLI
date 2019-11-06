c1a301b7c3579540a3e69f771d406aaa38ba73a6
Y.Ukrainets <y.ukrainets@viseven.com>
2019-11-06 16:53:28 +0200

    upd write function

--- src/write.js
+++ src/write.js
@@ -17,17 +17,18 @@ export function write(path, data) {
 
 function writeJSON(path, data) {
 	let file = JSON.parse(fs.readFileSync(path));
-
+	
 	if (Array.isArray(data)) {
 		data.forEach((element) => {
 			file[element.name] = element.content;
 		})
 	} else {
 		file[data.name] = data.content;
-	};
-
+	}
+	;
+	
 	let jsonString = JSON.stringify(file);
-
+	
 	fs.writeFileSync(path, jsonString, (err) => {
 		if (err) {
 			console.log(`Error writing file ${path}`, err)
@@ -38,18 +39,41 @@ function writeJSON(path, data) {
 function writeCSS(path, data) {
 	let file = css.parse(fs.readFileSync(path, 'utf8'));
 	let styles = file.stylesheet.rules;
-	let incomeStyle = css.parse(data).stylesheet.rules[0].declarations;
+	let incomeParsed = css.parse(data).stylesheet.rules[0];
+	let incomeStyles = css.parse(data).stylesheet.rules[0].declarations;
 	let incomeId = css.parse(data).stylesheet.rules[0].selectors[0];
-
-
-	styles.forEach((element) => {
-		if (element.selectors[0] == incomeId) {
-			incomeStyle.forEach((newStyle) => {
-				
-			})
-		}
-	});
-
+	
+	if (includeId(styles, incomeId)) {
+		console.log('present');
+		incomeStyles.forEach((newStyleItem) => {
+			// if (includeStyle(elementStyles.declarations, newStyleItem)) {
+			// 	return
+			// } else {
+			// 	file.stylesheet.rules[index].declarations.push(newStyleItem)
+			// }
+		})
+	}else{
+		console.log('Not present');
+		file.stylesheet.rules.push(incomeParsed);
+		console.log(file.stylesheet.rules);
+	}
+	// styles.forEach((elementStyles,index) => {
+	//
+	// 	if (elementStyles.selectors[0] == incomeId) {
+	// 		incomeStyles.forEach((newStyleItem) => {
+	//
+	// 			if( includeStyle(elementStyles.declarations,newStyleItem))
+	// 			{
+	// 				return
+	// 			}else{
+	// 				file.stylesheet.rules[index].declarations.push(newStyleItem)
+	// 			}
+	// 		})
+	// 	}
+	// });
+	
+	// console.log(css.stringify(file));
+	
 }
 
 function writeToEndOfFile(path, data) {
@@ -60,4 +84,14 @@ function writeToEndOfFile(path, data) {
 	})
 }
 
-function hasProperty(elementProp) { return elementProp.property == 'width' }
+function includeStyle(arr, style) {
+	return arr.some(function (arrVal) {
+		return arrVal.property == style.property;
+	});
+}
+
+function includeId(arr, id) {
+	return arr.some(function (arrVal) {
+		return arrVal.selectors[0] == id;
+	});
+}
