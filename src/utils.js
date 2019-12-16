94d89eb7c76f0fe1634d67c31a515d762dfc71a6
Y.Ukrainets <yuriyukrainets@gmail.com>
2019-12-16 23:44:50 +0200

    tr1

--- src/utils.js
+++ src/utils.js
@@ -8,12 +8,13 @@ const sharp = require('sharp'),
 
 export function isOdd(num) { return num % 2; }
 
-export function compress(id, name) {
+export async function compress(id, name) {
 	name ? name : `*.{jpg,png}`;
 	let file = [];
 	file.push(`./app/media/images/${id}/${name}`);
 
 	(async () => {
+		console.log(file);
 		const files = await imagemin(file, {
 			destination: `./app/media/images/${id}`,
 			plugins: [
@@ -33,85 +34,58 @@ export function getImage(id, name) {
 }
 
 export function updateDimensions(id, name) {
-	return new Promise((resolve, reject) => {
-		let image = sharp(`./app/media/images/${id}/${name}`);
-
-		image.metadata()
-			.then((metadata) => {
-				let changeWidth = isOdd(metadata.width);
-				let changeHeight = isOdd(metadata.height);
-				if (changeWidth && changeHeight) {
-					return image
-						.extend({
-							top: 0,
-							bottom: 1,
-							right: 1,
-							left: 0,
-							background: { r: 0, g: 0, b: 0, alpha: 0 }
-						})
-						.toFile(`./app/media/images/${id}/${name}temp`)
-				}
-				;
-				if (changeWidth && !changeHeight) {
-					return image
-						.extend({
-							top: 0,
-							bottom: 0,
-							right: 1,
-							left: 0,
-							background: { r: 0, g: 0, b: 0, alpha: 0 }
-						})
-						.toFile(`./app/media/images/${id}/${name}temp`)
-				}
-				;
-				if (!changeWidth && changeHeight) {
-					return image
-						.extend({
-							top: 0,
-							bottom: 1,
-							right: 0,
-							left: 0,
-							background: { r: 0, g: 0, b: 0, alpha: 0 }
-						})
-						.toFile(`./app/media/images/${id}/${name}temp`)
-				}
-				;
-				if (!changeWidth && !changeHeight) {
-					return new Promise((resolve, reject) => {
-						resolve();
+	let image = sharp(`./app/media/images/${id}/${name}`);
+	return image.metadata()
+		.then((metadata) => {
+			let changeWidth = isOdd(metadata.width);
+			let changeHeight = isOdd(metadata.height);
+			if (changeWidth || changeHeight) {
+				return image
+					.extend({
+						top: 0,
+						bottom: Number(changeHeight),
+						right: Number(changeWidth),
+						left: 0,
+						background: { r: 0, g: 0, b: 0, alpha: 0 }
 					})
-				}
-				;
-			})
-			.then(() => {
+					.toFile(`./app/media/images/${id}/${name}temp`)
+			} else {
 				return new Promise((resolve, reject) => {
-					fs.access(`./app/media/images/${id}/${name}temp`, fs.constants.F_OK, (err) => {
-						if (!err) {
-							fs.unlink(`./app/media/images/${id}/${name}`, (err) => {
-								if (err) throw err;
-							});
-							resolve();
-						}
-						resolve();
-					})
-				});
-			})
-			.then(() => {
-				return new Promise((resolve, reject) => {
-					fs.access(`./app/media/images/${id}/${name}temp`, fs.constants.F_OK, (err) => {
-						if (!err) {
-							fs.rename(`./app/media/images/${id}/${name}temp`, `./app/media/images/${id}/${name}`, (err) => {
-								if (err) {
-									throw err;
-									return reject()
-								}
-							});
-						}
-					})
-
 					resolve();
-				});
-			})
-	})
+				})
+			}
+
+		})
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				fs.access(`./app/media/images/${id}/${name}temp`, fs.constants.F_OK, (err) => {
+					if (!err) {
+						fs.unlink(`./app/media/images/${id}/${name}`, (err) => {
+							if (err) throw err;
+						});
+						resolve();
+					}
+
+				})
+			});
+		})
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				fs.access(`./app/media/images/${id}/${name}temp`, fs.constants.F_OK, (err) => {
+					if (!err) {
+						fs.rename(`./app/media/images/${id}/${name}temp`, `./app/media/images/${id}/${name}`, (err) => {
+							if (err) {
+								throw err;
+								return reject()
+							}
+						});
+					}
+					resolve();
+				})
+
+
+			});
+		})
+
 
 }
